5fa22fdbf35f518e898c3684dbc95772eea758d9
GrimReaperSam <fayezlahoud@gmail.com>
2017-02-10 17:32:53 +0200

    Moving to jekyll

---
+++ _projects/piano.md
@@ -0,0 +1,48 @@
+---
+layout: project_single
+image_path: /img/portfolio/piano-teacher.jpeg
+category: Freelance
+project_name: Piano Teacher
+project_link: /projects/piano-teacher.html
+---
+<div class="block-nav-work margBottom" markdown="1">
+  * [<i class="icon-left-open-mini"></i>](#)
+  * [<i class="icon-cancel"></i>](/projects.html)
+  * [<i class="icon-right-open-mini"></i>](#)
+</div>
+
+<div class="block-single margBottom clearfix" markdown="1">
+<div class="col-md-10 col-md-offset-1" markdown="1">
+
+{: .large-title .margBSSmall}
+# Project <br/> Piano Teacher
+
+{: .margBSSmall}
+The aim of this project was to create a piano teacher software/hardware that guides
+students using lights over the piano keys. I worked on the software part and created a server that handles
+the storage of all the midi files. I also built a small music marketplace where users can check available songs,
+listen to them and buy the lessons. Backend users were able to add new music files to the database,
+while users were able to view, and learn songs they have bought. I also created a prototype piano UI that opens
+a midi file and plays it to simulate the hardware itself.
+
+The video below shows an example playing Für Elise by Ludwig van Beethoven. The orange keys indicate the next key,
+and the green ones currently pressed key. The tempo can be adjusted, and hands/sounds can be toggled in case the
+user wants to practise a single hand accompany the melody of the other hand.
+
+---
+
+* Job: **Freelance developer**
+* Date : **Jan 2013 - May 2013**
+* Technologies: **JavaFx, Maven, Spring MVC, Hibernate, MIDI File Format**
+
+</div>
+</div>
+
+
+<div class="block-single margBottom clearfix">
+  <div class="col-md-10 col-md-offset-1">
+      <video controls>
+          <source src="img/piano.mp4" type="video/mp4">
+      </video>
+  </div>
+</div>
--- js/script.js
+++ js/script.js
@@ -3,131 +3,14 @@
 
 //------------------------------------- Waiting for the entire site to load ------------------------------------------------//
 
-jQuery(window).load(function() { 
-		jQuery("#loaderInner").fadeOut(); 
-		jQuery("#loader").delay(200).fadeOut("slow"); 
+jQuery(window).load(function() {
+		jQuery("#loaderInner").fadeOut();
+		jQuery("#loader").delay(200).fadeOut("slow");
 });
 
 $(document).ready(function(){
 
-//------------------------------------- View Router -------------------------------------------------//
-
-    var currentLink;
-    var swap = function(link) {
-        if (currentLink === link) {
-            return;
-        }
-        currentLink = link;
-        $("html, body").animate({ scrollTop: 0 });
-        $("#content-div").fadeOut(350, function() {
-            $("#content-div").empty().load(link).fadeIn(800);
-        });
-    };
-
-    var about = function () { swap('/about.html') };
-    var projects = function () { swap('/projects.html'); };
-    var blog = function () { swap('/blog.html') };
-    var contact = function () { swap('/contact.html'); };
-
-    var project = function(params) { swap('/projects/' + params.id + '.html'); };
-
-    var routes = {
-        'about': {callback: about, matcher: routeMatcher('about')},
-        'projects': {callback: projects, matcher: routeMatcher('projects')},
-        'blog': {callback: blog, matcher: routeMatcher('blog')},
-        'contact': {callback: contact, matcher: routeMatcher('contact')},
-        'projects/:id': {callback: project, matcher: routeMatcher('projects/:id')}
-    };
-
-    $(document).on("click", "a.director-link", function(evt) {
-        evt.preventDefault();
-        evt.stopPropagation();
-        var href = $(this).attr("href");
-
-        for (var route in routes) {
-            var params = routes[route].matcher.parse(href);
-            if (params != null) {
-                routes[route].callback(params);
-                return;
-            }
-        }
-    });
-    routes['about'].callback();
-
-
-
-//------------------------------------- Site slider ------------------------------------------------//
-
-$("#testimonial-carousel").owlCarousel({
-    navigation : false,
-    slideSpeed : 300,
-    paginationSpeed : 400,
-    responsiveRefreshRate : 200,
-    responsiveBaseWidth: window,
-    pagination: true,
-    singleItem: true
-});
-
-
-$("#block-slider").owlCarousel({
-    navigation : false,
-    slideSpeed : 300,
-    paginationSpeed : 400,
-    responsiveRefreshRate : 200,
-    responsiveBaseWidth: window,
-    pagination: false,
-    singleItem: true,
-	navigation:true,
-    navigationText: ["<span class='icon-left-open-big'></span>","<span class='icon-right-open-big'></span>"]
-});
-
-
-
-
-//------------------------------------- End site slider------------------------------------------------//
-
-
-
-
-
-//------------------------------------- Skills percentage setup------------------------------------------------//
-$(".percentage").each(function(){
-          var  width= $(this).text();
-          $(this).css("width", width).empty();
-});
-//------------------------------------- End skills percentage setup------------------------------------------------//
-
-
-
-
 //------------------------------------- Portfolio setup------------------------------------------------//
-
-$('.box').magnificPopup({
-					  type: 'image',
-					fixedContentPos: false,
-					fixedBgPos: false,
-					mainClass: 'mfp-no-margins mfp-with-zoom',
-					image: {
-						verticalFit: true
-					},
-					zoom: {
-						enabled: true,
-						duration: 300
-					}
-				});
-
-
-$('.popup-youtube, .popup-vimeo').magnificPopup({
-	disableOn: 700,
-	type: 'iframe',
-	mainClass: 'mfp-fade',
-	removalDelay: 160,
-	preloader: false,
-
-	fixedContentPos: false
-});
-
-
 /*Filtred portfolio*/
 $('.filter li a').on("click", function(e){
 
@@ -142,9 +25,6 @@ $('.filter li a').on("click", function(e){
 
         if (filters !== 'all') {
 
-
-
-
         var selected =  $(this).closest('.works').find('.item');
 
         for(var i = 0; i < selected.length; i++){
@@ -155,7 +35,6 @@ $('.filter li a').on("click", function(e){
 
         }
    }
-
 
 });
 
