33274ed7a811b1205cc9743e18cd9b7d648f5050
GrimReaperSam <fayezlahoud@gmail.com>
2017-02-13 16:16:10 +0200

    Build site at 2017-02-13 14:16:10 UTC

--- js/script.js
+++ js/script.js
@@ -1,71 +1,21 @@
 //------------------------------------- Toggle bibtex div ---------------------------------------//
 function toggleDiv(element){
- if(document.getElementById(element).style.display == 'none')
-  document.getElementById(element).style.display = 'block';
- else
-       document.getElementById(element).style.display = 'none';
+  if(document.getElementById(element).style.display == 'none') {
+    document.getElementById(element).style.display = 'block';
+  } else {
+    document.getElementById(element).style.display = 'none';
+  }
 }
 //------------------------------------- End Toggle  div ---------------------------------------//
-
 
 (function($) {
   "use strict";
 
 //------------------------------------- Waiting for the entire site to load ------------------------------------------------//
 
-jQuery(window).load(function() {
-		jQuery("#loaderInner").fadeOut();
-		jQuery("#loader").delay(200).fadeOut("slow");
-});
-
-$(document).ready(function(){
-
-//------------------------------------- Portfolio setup------------------------------------------------//
-/*Filtred portfolio*/
-$('.filter li a').on("click", function(e){
-
-		e.preventDefault();
-		$(this).addClass('active');
-		$(this).parent().siblings().find('a').removeClass('active');
-
-
-
-        var filters = $(this).attr('data-filter');
-        $(this).closest('.works').find('.item').removeClass('disable');
-
-        if (filters !== 'all') {
-
-        var selected =  $(this).closest('.works').find('.item');
-
-        for(var i = 0; i < selected.length; i++){
-
-        if (!selected.eq(i).hasClass(filters)) {
-                    selected.eq(i).addClass('disable');
-				}
-
-        }
-   }
-
-});
-
-
-//------------------------------------- End portfolio setup------------------------------------------------//
-
-
-
-
-//------------------------------------- Search input------------------------------------------------//
-
-	$('.search-form i').on("click", function(){
-		$(this).closest('.search-form').find('input[type="text"]').focus();
-		if($(this).closest('.search-form').find('input[type="text"]').val()){
-			$(this).closest('.search-form').find('input[type="submit"]').trigger('click');
-		}
-	});
-
-//------------------------------------- End search input------------------------------------------------//
-
-});
-
+  jQuery(window).load(function() {
+  		jQuery("#loaderInner").fadeOut();
+  		jQuery("#loader").delay(200).fadeOut("slow");
+  });
 
 })(jQuery);
