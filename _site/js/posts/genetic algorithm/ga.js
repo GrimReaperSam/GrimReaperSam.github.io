6e04e22e98529ec3d00bc781b7d0ccfd19403d6c
GrimReaperSam <fayezlahoud@gmail.com>
2017-02-22 15:32:30 +0100

    Adding Crossover Part 1. Build site at 2017-02-22 14:32:30 UTC

--- _site/js/posts/genetic algorithm/ga.js
+++ _site/js/posts/genetic algorithm/ga.js
@@ -11,21 +11,44 @@ $(document).ready(function() {
   var matchColor = "#ffffff";
   var nonMatchBgnd = "#33373d";
   var matchBgnd = "#558e6a";
+  var splitColor = "#cd5353";
   var width = 50;
   var height = 20;
 
-  var fitness = function(original, candidate) {
+  var randomWord = function() {
+    var rdWord = '';
+    _(word.length).times(function(idx)  {
+      rdWord += charset.charAt(Math.floor(Math.random() * charset.length))
+    });
+    return rdWord;
+  }
+
+  var fitness = function(candidate) {
     var fitness = 0;
 
     var i;
-    for (i = 0;i < original.length; ++i) {
-      if (original[i] == candidate[i]) {
+    for (i = 0;i < word.length; ++i) {
+      if (word[i] == candidate[i]) {
           fitness += 1;
       }
-      fitness += (127 - Math.abs(original.charCodeAt(i) - candidate.charCodeAt(i))) / 127;
+      fitness += (127 - Math.abs(word.charCodeAt(i) - candidate.charCodeAt(i))) / 127;
     }
 
     return Math.round(fitness * 100) / 100;
+  }
+
+  var setText = function(chromosome, text) {
+    chromosome.textattr("font-family", "'open_sansbold',Arial,sans-serif");
+    chromosome.textattr("fill", nonMatchColor);
+    chromosome.rectattr("fill", nonMatchBgnd);
+
+    _(word.length).times(function(idx) {
+      chromosome.setText(text[idx], idx);
+      if (text[idx] === word.charAt(idx)) {
+        chromosome.textattr("fill", matchColor, idx);
+        chromosome.rectattr("fill", matchBgnd, idx);
+      }
+    })
   }
   /////////////////////////
   // SETUP VARIABLES END //
@@ -38,54 +61,102 @@ $(document).ready(function() {
     .attr("viewBox", `0 0 ${(width + 1) * word.length} ${(height + 5) * 4}`);
 
   _(4).times(function(index) {
-      var dims = {"x": 0, "y": (height + 5) * index, "width": width, "height": height};
-      var chr = new Chromosome(svg, word.length, dims)
-      chr.textattr("font-family", "'open_sansbold',Arial,sans-serif");
-      chr.textattr("fill", nonMatchColor);
-      chr.rectattr("fill", nonMatchBgnd);
+    var dims = {"x": 0, "y": (height + 5) * index, "width": width, "height": height};
+    var populationChr = new Chromosome(svg, word.length, dims);
+    setText(populationChr, randomWord());
+  });
+  ////////////////////////////
+  // POPULATION EXAMPLE END //
+  ////////////////////////////
 
-      _(word.length).times(function(idx) {
-        var char = charset.charAt(Math.floor(Math.random() * charset.length))
-        chr.setText(char, idx);
-        if (char === word.charAt(idx)) {
-          chr.textattr("fill", matchColor, idx);
-          chr.rectattr("fill", matchBgnd, idx);
-        }
-      })
+
+  //////////////////////////////
+  // MATCHING 5 EXAMPLE START //
+  //////////////////////////////
+  var svg = d3.select("#matching-five-example").append("svg")
+    .attr("viewBox", `0 0 ${(width + 1) * word.length} ${height}`);
+  var matchSix = "H.plo!LoAndi";
+
+  var dims = {"x": 0, "y": 0, "width": width, "height": height};
+  var matchingFiveChr = new Chromosome(svg, word.length, dims);
+  setText(matchingFiveChr, matchSix);
+
+  $('#matching-five-fitness').html(fitness(word, matchSix));
+  $('#worle-fitness').text(fitness(word, "Hello Worle!"));
+  $('#worly-fitness').text(fitness(word, "Hello Worly!"));
+  ////////////////////////////
+  // MATCHING 5 EXAMPLE END //
+  ////////////////////////////
+
+
+  /////////////////////////////
+  // CROSSOVER EXAMPLE START //
+  /////////////////////////////
+  var distance = 30;
+  var svg = d3.select("#crossover-example").append("svg")
+    .attr("viewBox", `0 0 ${(width + 1) * word.length} ${height * 4 + distance * 4}`);
+  var dad = "Halli Bored!";
+  var mom = "Peibo Random";
+
+  var dims = {"x": 0, "y": 0, "width": width, "height": height};
+  var dadChr = new Chromosome(svg, word.length, dims)
+  setText(dadChr, dad);
+
+  var dims = {"x": 0, "y": distance + height, "width": width, "height": height};
+  var momChr = new Chromosome(svg, word.length, dims)
+  setText(momChr, mom);
+
+  // SINGLE POINT CROSSOVER //
+  var crossoverPoint = Math.floor(Math.random() * (word.length - 4)) + 2
+  svg.append("line")
+    .attr("stroke", splitColor)
+    .attr("stroke-width", 2)
+    .attr("x1", (width + 1) * crossoverPoint)
+    .attr("y1", 0)
+    .attr("x2", (width + 1) * crossoverPoint)
+    .attr("y2", distance + height * 2);
+
+  debugger
+  var firstChild = function() {
+    _.each(_.range(crossoverPoint), function(i) {
+      dadChr.getGene(i)
+        .transition()
+        .duration(2000)
+        .ease(d3.easeLinear)
+        .attr("transform", `translate(0, ${(height + distance) * 2 + distance})`)
+        .on("end", function() {
+          momChr.getGene(i)
+            .transition()
+            .duration(2000)
+            .ease(d3.easeLinear)
+            .attr("transform", `translate(0, ${height + distance * 2})`);
+        });
     });
-    ////////////////////////////
-    // POPULATION EXAMPLE END //
-    ////////////////////////////
-
-
-    //////////////////////////////
-    // MATCHING 5 EXAMPLE START //
-    //////////////////////////////
-    var svg = d3.select("#matching-five-example").append("svg")
-      .attr("viewBox", `0 0 ${(width + 1) * word.length} ${height}`);
-    var matchSix = "H.plo!LoAndi";
-
-    var dims = {"x": 0, "y": 0, "width": width, "height": height};
-    var chr = new Chromosome(svg, word.length, dims)
-    chr.textattr("font-family", "'open_sansbold',Arial,sans-serif");
-    chr.textattr("fill", nonMatchColor);
-    chr.rectattr("fill", nonMatchBgnd);
-
-    _(word.length).times(function(idx) {
-      var char = matchSix.charAt(idx)
-      chr.setText(char, idx);
-      if (char === word.charAt(idx)) {
-        chr.textattr("fill", matchColor, idx);
-        chr.rectattr("fill", matchBgnd, idx);
-      }
+  }
+  // firstChild();
+  var secondChild = function() {
+    _.each(_.range(crossoverPoint, word.length), function(i) {
+      dadChr.getGene(i)
+        .transition()
+        .duration(2000)
+        .ease(d3.easeLinear)
+        .attr("transform", `translate(0, ${(height + distance) * 3 + distance})`)
+        .on("end", function() {
+          momChr.getGene(i)
+            .transition()
+            .duration(2000)
+            .ease(d3.easeLinear)
+            .attr("transform", `translate(0, ${(height + distance) * 2 + distance })`)
+            .on("end", function() {
+              secondChild();
+            });
+        });
     });
+  };
 
-    $('#matching-five-fitness').html(fitness(word, matchSix));
-    $('#worle-fitness').text(fitness(word, "Hello Worle!"));
-    $('#worly-fitness').text(fitness(word, "Hello Worly!"));
-    ////////////////////////////
-    // MATCHING 5 EXAMPLE END //
-    ////////////////////////////
+  /////////////////////////////
+  // CROSSOVER 5 EXAMPLE END //
+  /////////////////////////////
 });
 
 
--- js/posts/genetic algorithm/chromosome.js
+++ js/posts/genetic algorithm/chromosome.js
@@ -6,7 +6,6 @@ Number.prototype.times = function(func) {
 
 function Chromosome(svg, size, dims) {
     this.size = size;
-    this.genes = [];
     this.dims = dims;
 
     this.svg = svg.append("g")
@@ -27,40 +26,33 @@ function Chromosome(svg, size, dims) {
         .attr("dy", ".35em")
         .attr("text-anchor", "middle")
         .text(`Gene (${i})`);
-
-      this.genes.push(gene);
     }.bind(this));
+    
+    this.rects = this.svg.selectAll("g rect");
+    this.texts = this.svg.selectAll("g text");
 
     this.setText = function(value, index) {
       if (index == undefined) {
-        this.svg.selectAll("text").text(value);
+        this.texts.text(value);
       } else {
-        this.genes[index].selectAll("text").text(value);
+        d3.select(this.texts._groups[0][index]).text(value);
       }
     }
 
     this.attr = function(name, value, object, index) {
       if (index === undefined) {
-        this.svg.selectAll(object).attr(name, value);
+        object.attr(name, value);
       } else {
-        this.genes[index].selectAll(object).attr(name, value);
+        d3.select(object._groups[0][index]).attr(name, value);
       }
     }
 
     this.textattr = function(name, value, index) {
-        this.attr(name, value, 'text', index);
+        this.attr(name, value, this.texts, index);
     }
 
     this.rectattr = function(name, value, index) {
-      this.attr(name, value, 'rect', index);
-    }
-
-    this.getGenes = function() {
-      return this.genes;
-    }
-
-    this.getGene = function(index) {
-      return this.genes[index];
+      this.attr(name, value, this.rects, index);
     }
 
 }
