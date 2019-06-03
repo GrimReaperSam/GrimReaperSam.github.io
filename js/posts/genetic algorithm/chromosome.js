857df61e86a19e1778513721535e0319dbed429f
GrimReaperSam <fayezlahoud@gmail.com>
2019-06-03 23:27:21 +0200

    Pix4D + new papers. Build site at 2019-06-03 21:27:21 UTC

--- js/posts/genetic algorithm/chromosome.js
+++ js/posts/genetic algorithm/chromosome.js
@@ -2,71 +2,63 @@ Number.prototype.times = function(func) {
     for(var i = 0; i < Number(this); i++) {
         func(i);
     }
-}
+};
 
-function Chromosome(svg, size, dims) {
-    this.size = size;
+const fontFamily = "'open_sansbold',Arial,sans-serif";
+const nonMatchColor = "#85878b";
+const matchColor = "#ffffff";
+const nonMatchBgnd = "#33373d";
+const matchBgnd = "#558e6a";
+const splitColor = "#cd5353";
+
+const goal = 'Hello World!';
+const width = 50;
+const height = 20;
+
+let update = function(chr) {
+    chr.gene.selectAll('rect').data(chr.text).enter()
+        .append('g').append("rect")
+        .attr("x", (d, i) => (width + 1) * i)
+        .attr("y", 0)
+        .attr("width", width)
+        .attr("height", height)
+        .attr('fill', (d, i) => d === goal[i] ? matchBgnd: nonMatchBgnd);
+
+    chr.gene.selectAll('g').append("text")
+        .attr("x", (d, i) => (width + 1) / 2 + (width + 1) * i)
+        .attr("y", height / 2)
+        .attr("dy", ".35em")
+        .attr("text-anchor", "middle")
+        .attr('font-family', fontFamily)
+        .attr('fill', (d, i) => d === goal[i] ? matchColor: nonMatchColor)
+        .text((d) => d);
+};
+
+let move = function(chr, x, y) {
+    chr.dims.x += x;
+    chr.dims.y += y;
+    chr.group.transition().duration(1000).attr("transform", `translate(${chr.dims.x}, ${chr.dims.y})`);
+};
+
+function Chromosome(svg, dims, text) {
     this.dims = dims;
+    this.text = typeof (text) === "string" ? text.split(''): text;
+
 
     this.svg = svg;
     this.group = this.svg.append("g")
               .attr("transform", `translate(${this.dims.x}, ${this.dims.y})`);
 
-    this.size.times(function(i) {
-      var gene = this.group.append("g");
+    this.gene = this.group.append("g");
 
-      gene.append("rect")
-        .attr("x", (this.dims.width + 1) * i)
-        .attr("y", 0)
-        .attr("width", this.dims.width)
-        .attr("height", this.dims.height);
+    update(this);
+}
 
-      gene.append("text")
-        .attr("x", (this.dims.width + 1) / 2 + (this.dims.width + 1) * i)
-        .attr("y", this.dims.height / 2)
-        .attr("dy", ".35em")
-        .attr("text-anchor", "middle")
-        .text(`Gene (${i})`);
-    }.bind(this));
+function ChromosomeCrossover(svg, a, b, separator) {
+    let firstChildP1 = new Chromosome(svg, a.dims, a.text.slice(0, separator));
+    move(firstChildP1, 0, 2 * (30 + height));
 
-    this.genes = this.group.selectAll("g");
-    this.rects = this.genes.selectAll("rect");
-    this.texts = this.genes.selectAll("text");
-
-    this.setText = function(value, index) {
-      if (index == undefined) {
-        this.genes.selectAll("text").text(value);
-      } else {
-        this.getGene(index).select("text").text(value);
-      }
-    }
-
-    this.attr = function(name, value, object, index) {
-      if (index === undefined) {
-        this.genes.selectAll(object).attr(name, value);
-      } else {
-        this.getGene(index).select(object).attr(name, value);
-      }
-    }
-
-    this.textattr = function(name, value, index) {
-        this.attr(name, value, "text", index);
-    }
-
-    this.rectattr = function(name, value, index) {
-      this.attr(name, value, "rect", index);
-    }
-
-    this.getGene = function(index) {
-      return this.genes.filter(function(d, i) {return i === index});
-    }
-
-    this.clone = function() {
-      return new Chromosome(this.svg, this.size, this.dims);
-    }
-
-    this.remove  = function() {
-      this.genes.remove();
-    }
-
+    let f2dims = {'x': (width+1)*separator, y:30 + height};
+    let firstChildP2 = new Chromosome(svg, f2dims, b.text.slice(separator));
+    move(firstChildP2, 0, 30 + height);
 }
